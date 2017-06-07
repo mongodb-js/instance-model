2ce0ee7421f1612dd3d805a62e2fc347aeccf8ac
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2017-06-07 16:47:07 -0400

    Use MONGODB_URL env var if set for functional tests

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -7,9 +7,18 @@ var fetch = require('../').fetch;
 var runner = require('mongodb-runner');
 var debug = require('debug')('mongodb-instance-model:test:fetch');
 
-var fixtures = require('mongodb-connection-fixture').MATRIX.map(function(model) {
+var fixtures = require('mongodb-connection-fixture').MATRIX.map(function(
+  model
+) {
   return new Connection(model);
 });
+
+if (process.env.MONGODB_URL) {
+  var connectionFromEnvVar = Connection.from(process.env.MONGODB_URL);
+  connectionFromEnvVar.name = 'MONGODB_URL environment variable';
+  fixtures.push(connectionFromEnvVar);
+}
+
 describe('mongodb-instance-model#fetch', function() {
   describe('local', function() {
     var db;
@@ -42,7 +51,9 @@ describe('mongodb-instance-model#fetch', function() {
         done();
       });
     });
-    it('should not close the db after getting instance details', function(done) {
+    it('should not close the db after getting instance details', function(
+      done
+    ) {
       assert(db);
       fetch(db, function(err) {
         if (err) {
@@ -63,7 +74,9 @@ describe('mongodb-instance-model#fetch', function() {
    * will be able to properly spin up deployments w authentication.
    */
   it.skip('should get instance details for john doe', function(done) {
-    var connection = Connection.from('john:doe@localhost:30000/admin?authMechanism=MONGODB-CR');
+    var connection = Connection.from(
+      'john:doe@localhost:30000/admin?authMechanism=MONGODB-CR'
+    );
     connect(connection, function(err, db) {
       if (err) {
         return done(err);
@@ -80,50 +93,56 @@ describe('mongodb-instance-model#fetch', function() {
 
   if (fixtures.length > 0) {
     describe('functional #slow', function() {
-      _.map(_.groupBy(fixtures, 'authentication'), function(models, authentication) {
-        describe(format('Using authentication `%s`', authentication), function() {
-          _.each(models, function(model) {
-            describe(model.name, function() {
-              var db;
-              it('should connect', function(done) {
-                if (process.env.dry) {
-                  this.skip();
-                  return;
-                }
-                this.slow(5000);
-                this.timeout(20000);
-
-                connect(model, function(err, _db) {
-                  if (err) {
-                    return done(err);
+      _.map(_.groupBy(fixtures, 'authentication'), function(
+        models,
+        authentication
+      ) {
+        describe(
+          format('Using authentication `%s`', authentication),
+          function() {
+            _.each(models, function(model) {
+              describe(model.name, function() {
+                var db;
+                it('should connect', function(done) {
+                  if (process.env.dry) {
+                    this.skip();
+                    return;
                   }
-                  db = _db;
-                  done();
-                });
-              });
-              it('should get instance details', function(done) {
-                if (process.env.dry) {
-                  this.skip();
-                  return;
-                }
+                  this.slow(5000);
+                  this.timeout(20000);
 
-                this.slow(5000);
-                this.timeout(10000);
-                assert(db, 'requires successful connection');
-                fetch(db, function(err, res) {
-                  debug('got instance details', res);
-                  done(err, res);
+                  connect(model, function(err, _db) {
+                    if (err) {
+                      return done(err);
+                    }
+                    db = _db;
+                    done();
+                  });
                 });
-              });
+                it('should get instance details', function(done) {
+                  if (process.env.dry) {
+                    this.skip();
+                    return;
+                  }
 
-              after(function() {
-                if (db) {
-                  db.close();
-                }
+                  this.slow(5000);
+                  this.timeout(10000);
+                  assert(db, 'requires successful connection');
+                  fetch(db, function(err, res) {
+                    debug('got instance details', res);
+                    done(err, res);
+                  });
+                });
+
+                after(function() {
+                  if (db) {
+                    db.close();
+                  }
+                });
               });
             });
-          });
-        });
+          }
+        );
       });
     });
   }
